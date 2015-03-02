542b09a93b925a7e8b58b00acc94233031a6f4d0
hvelarde <hector.velarde@gmail.com>
2015-03-02 18:17:22 -0300

    Flake8

--- src/collective/z3cform/aceeditorwidget/__init__.py
+++ src/collective/z3cform/aceeditorwidget/__init__.py
@@ -6,4 +6,4 @@ from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('collective.z3cform.aceeditorwidget')
 
 
-from .aceeditor import AceEditorFieldWidget # noqa
+from .aceeditor import AceEditorFieldWidget  # noqa
